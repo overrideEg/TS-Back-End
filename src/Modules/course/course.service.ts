66fbe9d808bbe6837dbe4a83964f01f5438e41cb
Abkholy <alkholyabdelrahman@gmail.com>
2021-05-18 14:33:35 +0200

    changes in course

--- src/Modules/course/course.service.ts
+++ src/Modules/course/course.service.ts
@@ -9,8 +9,7 @@ import { TeacherService } from '../teacher/teacher.service';
 import { UserService } from '../user/user.service';
 @Injectable()
 export class CourseService {
-
-
+ 
 
 
     constructor(
@@ -29,6 +28,36 @@ export class CourseService {
 
         return this.CourseModel.create(body)
     }
+    async update(req: any, id: string, body: Course): Promise<Course | PromiseLike<Course>> {
+        if (req.user.userType !== UserType.teacher.toString()) {
+            throw new BadRequestException('only teacher can update courses');
+        }
+        let teacher = await this.userService.findOne(req.user.id);
+        if (teacher['_id'].toString !== req.user.id){
+            throw new BadRequestException('only teacher can update his courses');
+        }
+        //TODO:
+        await this.CourseModel.findByIdAndUpdate(id,body).exec();
+        return await this.CourseModel.findById(id).exec();
+    }
+
+    async delete(req: any, id: string): Promise<Course | PromiseLike<Course>> {
+        if (req.user.userType !== UserType.teacher.toString()) {
+            throw new BadRequestException('only teacher can update courses');
+        }
+        let teacher = await this.userService.findOne(req.user.id);
+        if (teacher['_id'].toString !== req.user.id){
+            throw new BadRequestException('only teacher can update his courses');
+        }
+        let course = await this.CourseModel.findById(id).exec();
+        if (course.startDate < Date.now()){
+            throw new BadRequestException('you can not delete started course');
+        }
+
+        return await this.CourseModel.findByIdAndDelete(id);
+      }
+   
+
 
 
     async addCourseContent(req: any, courseId: string, contents: CourseContent[]): Promise<CourseContent[] | PromiseLike<CourseContent[]>> {
@@ -44,12 +73,12 @@ export class CourseService {
             content.lessons.forEach(lesson => lesson['OId'] = OverrideUtils.generateGUID());
         })
         course.content = contents;
-        await course.updateOne({_id : course['_id']}).exec();
+        await course.updateOne({ _id: course['_id'] }).exec();
         return course.content;
     }
 
 
-   
+
 
     async getTeacherCourses(req: any): Promise<Course[] | PromiseLike<Course[]>> {
         if (req.user.userType !== UserType.teacher.toString()) {
@@ -57,17 +86,9 @@ export class CourseService {
         }
         let teacher = (await this.userService.findOne(req.user.id)).teacher;
         let courses = await this.CourseModel.find({ teacher: teacher['_id'] })
-        .populate('stage').populate('grade').populate({
-            path:'grade',
-            populate:{
-                path: "stage",
-                model:"Stage"
-             }
-        })
-            .lean()
-            .exec()
+            .exec() as any
         courses.forEach(course => {
-
+            course = course.toObject();
             course.cRating = 10 - Math.random() * 10
             course.progress = 100 - Math.random() * 100
         })
