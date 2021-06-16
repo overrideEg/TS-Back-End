68eb0a4797775aebef8bb76afc31f664e3772d86
Abkholy <alkholyabdelrahman@gmail.com>
2021-06-16 17:45:17 +0200

    forward ref

--- src/Models/student.model.ts
+++ src/Models/student.model.ts
@@ -31,7 +31,7 @@ export class Student extends OBaseEntity {
     grade?: Grade;
 
     @ApiProperty({ type: () => User })
-    @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User',autopopulate: true })
+    @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User' })
     user?: User;
     
 
