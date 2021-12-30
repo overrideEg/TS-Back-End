69bbb6a291b6addb07167e8a2dad90df48a32c14
Abkholy <alkholyabdelrahman@gmail.com>
2021-12-30 23:45:00 +0200

    Fixes

--- src/api-modules/search/search.service.ts
+++ src/api-modules/search/search.service.ts
@@ -137,8 +137,8 @@ export class SearchService {
       profile.noOfReviews = latestFeedback.length;
       profile.rate =
         latestFeedback.length > 0
-          ? latestFeedback.reduce((acc, feedBack) => acc + feedBack.stars, 0) /
-          profile.noOfCourses
+          ? (latestFeedback.reduce((acc, feedBack) => acc + feedBack.stars, 0) /
+            profile.noOfCourses)
           : 5;
 
       profile.name = course.teacher.name;
